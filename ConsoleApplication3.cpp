f779d13af9161b5ea6ac25f06318f163c908c8f4
AndreyZinovyev <Andreyvspc@yandex.ru>
2021-05-18 17:07:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int zadanie_1(int N, int p)
+{
+	if (N == 1 || N == 0)
+	{
+		if (N == 1)
+			return N;
+		if (N == 0)
+			return N;
+	}
+	int* SS = new int[10];
+	int END = 0;
+	int i = 0;
+	while (END == 0)
+	{
+		SS[i] = N % p;
+		N = N / p;
+		i++;
+		if (N < p)
+		{
+			SS[i] = N;
+			END = i;
+		}
+	}
+	for (int i = 0; i <= END; i++)
+	{
+		cout << SS[i];
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	zadanie_1(10, 2);
+}
